2c6e5b1b0e8a9707d7200497023064c0989a5c16
leightonburgoyne0 <91318744+leightonburgoyne0@users.noreply.github.com>
2021-10-01 11:09:35 +0100

    Edited Credits

--- app211-enrollments/Student.java
+++ app211-enrollments/Student.java
@@ -4,8 +4,8 @@
  * concerning the course the student is currently enrolled on
  * 
  * @author Michael Kölling and David Barnes
- * @modified by Derek Peacock and Nicholas Day
- * @version 2021:08:15
+ * @modified by Leighton Burgoyne
+ * @version v1.0
  */
 public class Student
 {
